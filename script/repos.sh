a8bece377007a37dbfc9eb0bceb92aae03ea7f28
Ammon Casey <ammon@caseyohana.com>
2014-12-19 13:06:59 -1000

    removed postgres and hhvm repos

--- script/repos.sh
+++ script/repos.sh
@@ -8,8 +8,8 @@ cd /tmp
 wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
 wget http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
-wget http://yum.postgresql.org/9.3/redhat/rhel-6-x86_64/pgdg-centos93-9.3-1.noarch.rpm
-wget http://yum.gleez.com/6/x86_64/gleez-repo-6-0.el6.noarch.rpm
+# wget http://yum.postgresql.org/9.3/redhat/rhel-6-x86_64/pgdg-centos93-9.3-1.noarch.rpm
+# wget http://yum.gleez.com/6/x86_64/gleez-repo-6-0.el6.noarch.rpm
 rpm --import http://apt.sw.be/RPM-GPG-KEY.dag.txt
-sudo rpm -Uvh remi-release-6*.rpm epel-release-6*.rpm rpmforge-release*.rpm pgdg*.rpm gleez*.rpm
+sudo rpm -Uvh remi-release-6*.rpm epel-release-6*.rpm rpmforge-release*.rpm
 
