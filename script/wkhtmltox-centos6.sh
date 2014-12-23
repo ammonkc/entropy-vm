f3b9e5e0a7fc2a515ae9ffc8175450a4b3b97664
Ammon Casey <ammon@caseyohana.com>
2014-12-23 10:38:39 -1000

    adds -y flag on wkhtmltox install command

--- script/wkhtmltox-centos6.sh
+++ script/wkhtmltox-centos6.sh
@@ -4,4 +4,4 @@ echo '==> Installing wkhtmltopdf'
 cd /tmp
 wget http://downloads.sourceforge.net/project/wkhtmltopdf/0.12.1/wkhtmltox-0.12.1_linux-centos6-amd64.rpm
 yum --enablerepo=remi -y install urw-fonts libXext openssl-devel
-yum --nogpgcheck localinstall wkhtmltox*.rpm
+yum --nogpgcheck -y localinstall wkhtmltox*.rpm
