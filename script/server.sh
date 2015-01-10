894f467fb7efe3b7139e59426a0451783a5c58f5
Ammon Casey <ammon@caseyohana.com>
2015-01-09 23:54:41 -1000

    fix iptables command

--- script/server.sh
+++ script/server.sh
@@ -151,5 +151,5 @@ iptables -I INPUT -p tcp --dport 80 -j ACCEPT
 iptables -I INPUT -p tcp --dport 443 -j ACCEPT
 iptables -I INPUT -p tcp --dport 3306 -j ACCEPT
 iptables -I INPUT -p tcp --dport 8081 -j ACCEPT
-iptables -A INPUT -p tcp --dport 53 -j ACCEPT
+iptables -I INPUT -p tcp --dport 53 -j ACCEPT
 service iptables save
