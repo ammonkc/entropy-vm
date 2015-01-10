e60686841294bdfad619a1f508c8d72cd7c9e22e
Ammon Casey <ammon@caseyohana.com>
2015-01-09 22:49:41 -1000

    remves hbp comments

--- script/h5bp.sh
+++ script/h5bp.sh
@@ -3,72 +3,14 @@
 echo "==> Installing html5-boilerplate apache configs"
 
 cat <<EOF > /etc/httpd/conf.d/h5bp.conf
-# ######################################################################
-# # CROSS-ORIGIN                                                       #
-# ######################################################################
-
-<IfModule mod_setenvif.c>
-    <IfModule mod_headers.c>
-        <FilesMatch "\.(bmp|cur|gif|ico|jpe?g|png|svgz?|webp)$">
-            SetEnvIf Origin ":" IS_CORS
-            Header set Access-Control-Allow-Origin "*" env=IS_CORS
-        </FilesMatch>
-    </IfModule>
-</IfModule>
-
-<IfModule mod_headers.c>
-    <FilesMatch "\.(eot|otf|tt[cf]|woff2?)$">
-        Header set Access-Control-Allow-Origin "*"
-    </FilesMatch>
-</IfModule>
-
-# ######################################################################
-# # ERRORS                                                             #
-# ######################################################################
-
-Options -MultiViews
-
-# ######################################################################
-# # INTERNET EXPLORER                                                  #
-# ######################################################################
-
-<IfModule mod_headers.c>
-    Header set X-UA-Compatible "IE=edge"
-    # `mod_headers` cannot match based on the content-type, however,
-    # the `X-UA-Compatible` response header should be send only for
-    # HTML documents and not for the other resources.
-    <FilesMatch "\.(appcache|atom|bbaw|bmp|crx|css|cur|eot|f4[abpv]|flv|geojson|gif|htc|ico|jpe?g|js|json(ld)?|m4[av]|manifest|map|mp4|oex|og[agv]|opus|otf|pdf|png|rdf|rss|safariextz|svgz?|swf|topojson|tt[cf]|txt|vcard|vcf|vtt|webapp|web[mp]|woff2?|xloc|xml|xpi)$">
-        Header unset X-UA-Compatible
-    </FilesMatch>
-</IfModule>
-
-# ######################################################################
-# # MEDIA TYPES AND CHARACTER ENCODINGS                                #
-# ######################################################################
-
 <IfModule mod_mime.c>
-
-  # Data interchange
-
     AddType application/json                            json map topojson
     AddType application/ld+json                         jsonld
     AddType application/vnd.geo+json                    geojson
     AddType application/xml                             atom rdf rss xml
-
-  # JavaScript
-
-    # Normalize to standard type.
-    # https://tools.ietf.org/html/rfc4329#section-7.2
-
     AddType application/javascript                      js
-
-  # Manifest files
-
     AddType application/x-web-app-manifest+json         webapp
     AddType text/cache-manifest                         appcache manifest
-
-  # Media files
-
     AddType audio/mp4                                   f4a f4b m4a
     AddType audio/ogg                                   oga ogg opus
     AddType image/bmp                                   bmp
@@ -78,32 +20,12 @@ Options -MultiViews
     AddType video/ogg                                   ogv
     AddType video/webm                                  webm
     AddType video/x-flv                                 flv
-
-    # Serving `.ico` image files with a different media type
-    # prevents Internet Explorer from displaying then as images:
-    # https://github.com/h5bp/html5-boilerplate/commit/37b5fec090d00f38de64b591bcddcb205aadf8ee
-
     AddType image/x-icon                                cur ico
-
-  # Web fonts
-
     AddType application/font-woff                       woff
     AddType application/font-woff2                      woff2
     AddType application/vnd.ms-fontobject               eot
-
-    # Browsers usually ignore the font media types and simply sniff
-    # the bytes to figure out the font type.
-    # https://mimesniff.spec.whatwg.org/#matching-a-font-type-pattern
-    #
-    # However, Blink and WebKit based browsers will show a warning
-    # in the console if the following font types are served with any
-    # other media types.
-
     AddType application/x-font-ttf                      ttc ttf
     AddType font/opentype                               otf
-
-  # Other
-
     AddType application/octet-stream                    safariextz
     AddType application/x-bb-appworld                   bbaw
     AddType application/x-chrome-extension              crx
@@ -113,12 +35,9 @@ Options -MultiViews
     AddType text/vnd.rim.location.xloc                  xloc
     AddType text/vtt                                    vtt
     AddType text/x-component                            htc
-
 </IfModule>
 
 AddDefaultCharset utf-8
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 <IfModule mod_mime.c>
     AddCharset utf-8 .atom \
@@ -137,31 +56,10 @@ AddDefaultCharset utf-8
                      .xml
 </IfModule>
 
-
-# ######################################################################
-# # REWRITES                                                           #
-# ######################################################################
-
 <IfModule mod_rewrite.c>
-
-    # (1)
     RewriteEngine On
-
-    # (2)
     Options +FollowSymlinks
-
-    # (3)
-    # Options +SymLinksIfOwnerMatch
-
-    # (4)
-    # RewriteBase /
-
-    # (5)
-    # RewriteOptions <options>
-
 </IfModule>
-
-# Option 1: rewrite www.example.com → example.com
 
 <IfModule mod_rewrite.c>
     RewriteEngine On
@@ -170,32 +68,9 @@ AddDefaultCharset utf-8
     RewriteRule ^ http://%1%{REQUEST_URI} [R=301,L]
 </IfModule>
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-# Option 2: rewrite example.com → www.example.com
-#
-# Be aware that the following might not be a good idea if you use "real"
-# subdomains for certain parts of your website.
-
-# <IfModule mod_rewrite.c>
-#     RewriteEngine On
-#     RewriteCond %{HTTPS} !=on
-#     RewriteCond %{HTTP_HOST} !^www\. [NC]
-#     RewriteCond %{SERVER_ADDR} !=127.0.0.1
-#     RewriteCond %{SERVER_ADDR} !=::1
-#     RewriteRule ^ http://www.%{HTTP_HOST}%{REQUEST_URI} [R=301,L]
-# </IfModule>
-
-
-# ######################################################################
-# # SECURITY                                                           #
-# ######################################################################
-
 <IfModule mod_autoindex.c>
     Options -Indexes
 </IfModule>
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 <IfModule mod_rewrite.c>
     RewriteEngine On
@@ -205,38 +80,25 @@ AddDefaultCharset utf-8
     RewriteRule "(^|/)\." - [F]
 </IfModule>
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 <FilesMatch "(^#.*#|\.(bak|conf|dist|fla|in[ci]|log|psd|sh|sql|sw[op])|~)$">
-
-    # Apache < 2.3
     <IfModule !mod_authz_core.c>
         Order allow,deny
         Deny from all
         Satisfy All
     </IfModule>
 
-    # Apache ≥ 2.3
     <IfModule mod_authz_core.c>
         Require all denied
     </IfModule>
-
 </FilesMatch>
 
 <IfModule mod_headers.c>
     Header set X-Content-Type-Options "nosniff"
 </IfModule>
 
-ServerTokens Prod
-
-# ######################################################################
-# # WEB PERFORMANCE                                                    #
-# ######################################################################
+ServerTokens Pro
 
 <IfModule mod_deflate.c>
-
-    # Force compression for mangled `Accept-Encoding` request headers
-    # https://developer.yahoo.com/blogs/ydn/pushing-beyond-gzipping-25601.html
 
     <IfModule mod_setenvif.c>
         <IfModule mod_headers.c>
@@ -244,8 +106,6 @@ ServerTokens Prod
             RequestHeader append Accept-Encoding "gzip,deflate" env=HAVE_Accept-Encoding
         </IfModule>
     </IfModule>
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     <IfModule mod_filter.c>
         AddOutputFilterByType DEFLATE "application/atom+xml" \
@@ -280,20 +140,13 @@ ServerTokens Prod
                                       "text/x-component" \
                                       "text/x-cross-domain-policy" \
                                       "text/xml"
-
     </IfModule>
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     <IfModule mod_mime.c>
         AddEncoding gzip              svgz
     </IfModule>
 
 </IfModule>
-
-# ----------------------------------------------------------------------
-# | Expires headers                                                    |
-# ----------------------------------------------------------------------
 
 <filesMatch "\.(html|htm|js|css)$">
   FileETag None
