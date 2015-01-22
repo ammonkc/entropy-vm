24a020816c71792fbec4f29239168563849976dd
Ammon Casey <ammon@caseyohana.com>
2015-01-22 09:39:46 -1000

    wkhtmltox udpated to 2.12.1

--- script/wkhtmltox.sh
+++ script/wkhtmltox.sh
@@ -4,11 +4,11 @@ echo '==> Installing wkhtmltopdf'
 cd /tmp
 
 if grep -q -i "release 6" /etc/redhat-release ; then
-    wget http://downloads.sourceforge.net/project/wkhtmltopdf/0.12.2/wkhtmltox-0.12.2_linux-centos6-amd64.rpm
+    wget http://downloads.sourceforge.net/project/wkhtmltopdf/0.12.2.1/wkhtmltox-0.12.2.1_linux-centos6-amd64.rpm
 fi
 
 if grep -q -i "release 7" /etc/redhat-release ; then
-    wget http://downloads.sourceforge.net/project/wkhtmltopdf/0.12.2/wkhtmltox-0.12.2_linux-centos7-amd64.rpm
+    wget http://downloads.sourceforge.net/project/wkhtmltopdf/0.12.2.1/wkhtmltox-0.12.2.1_linux-centos7-amd64.rpm
 fi
 
 yum --enablerepo=remi -y install urw-fonts libXext openssl-devel
