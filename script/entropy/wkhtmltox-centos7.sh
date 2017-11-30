3cd80da09146a32d59205a72ce697ea89c054c80
Ammon Casey <ammon@caseyohana.com>
2017-11-30 11:24:58 -1000

    [wkhtmltopdf] changed rpm download url to github.com

--- script/entropy/wkhtmltox-centos7.sh
+++ script/entropy/wkhtmltox-centos7.sh
@@ -2,6 +2,6 @@
 
 echo '==> Installing wkhtmltopdf'
 cd /tmp
-wget http://downloads.sourceforge.net/project/wkhtmltopdf/0.12.2.1/wkhtmltox-0.12.2.1_linux-centos6-amd64.rpm
+wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.2.1/wkhtmltox-0.12.2.1_linux-centos7-amd64.rpm
 yum --enablerepo=remi -y install urw-fonts libXext openssl-devel
 yum --nogpgcheck -y localinstall wkhtmltox*.rpm
