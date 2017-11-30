5bdf550a4e2ca3975271a60ede05f41c3b269cb5
Ammon Casey <ammon@caseyohana.com>
2017-11-30 13:04:57 -1000

    [util] update util repo to remi-php71

--- script/entropy/utils.sh
+++ script/entropy/utils.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -eux
 
 echo '==> Installing utilities'
-yum --enablerepo=remi,remi-php56 -y install gcc cpp make curl kernel-devel kernel-headers perl nfs-utils rpcbind vim screen sendmail postfix git-core zsh
+yum --enablerepo=remi,remi-php71 -y install gcc cpp make curl kernel-devel kernel-headers perl nfs-utils rpcbind vim screen sendmail postfix git-core zsh
