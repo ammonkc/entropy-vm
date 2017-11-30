5697c2eba7ac026b83819e8b1a9ef3c3386ea765
Ammon Casey <ammon@caseyohana.com>
2017-11-30 11:04:35 -1000

    [server|repo] update to latest postgresql v 9.6

--- script/entropy/repos.sh
+++ script/entropy/repos.sh
@@ -6,16 +6,14 @@ yum -y install wget
 cd /tmp
 
 if grep -q -i "release 6" /etc/redhat-release ; then
-    wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
     wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
     wget -O /etc/yum.repos.d/hop5.repo http://www.hop5.in/yum/el6/hop5.repo
     wget http://yum.postgresql.org/9.4/redhat/rhel-6-x86_64/pgdg-centos94-9.4-1.noarch.rpm
 fi
 
 if grep -q -i "release 7" /etc/redhat-release ; then
-    wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
     wget http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
-    wget http://yum.postgresql.org/9.4/redhat/rhel-7-x86_64/pgdg-centos94-9.4-1.noarch.rpm
+    wget https://download.postgresql.org/pub/repos/yum/9.6/redhat/rhel-7-x86_64/pgdg-centos96-9.6-3.noarch.rpm
 fi
 
 yum -y install epel-release
--- script/entropy/server.sh
+++ script/entropy/server.sh
@@ -82,13 +82,13 @@ mysql -e "GRANT ALL ON *.* TO 'entropy'@'localhost' WITH GRANT OPTION; UPDATE my
 mysql -e "GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION; UPDATE mysql.user SET Password = PASSWORD('Dbr00+') WHERE User='root'; FLUSH PRIVILEGES;" > /dev/null 2>&1
 
 echo "==> Installing postgreSQL"
-yum -y install postgresql94-server postgresql94-contrib
-service postgresql-9.4 initdb
-chkconfig postgresql-9.4 --add
-chkconfig postgresql-9.4 on --level 2345
-sed -i "s|host    all             all             127.0.0.1/32            ident|host    all             all             127.0.0.1/32            md5|" /var/lib/pgsql/9.4/data/pg_hba.conf
-sed -i "s|host    all             all             ::1/128                 ident|host    all             all             ::1/128                 md5|" /var/lib/pgsql/9.4/data/pg_hba.conf
-echo -e "host    all             all             10.0.2.2/32               md5" >> /var/lib/pgsql/9.4/data/pg_hba.conf
+yum -y install postgresql96-server postgresql96-contrib
+service postgresql-9.6 initdb
+chkconfig postgresql-9.6 --add
+chkconfig postgresql-9.6 on --level 2345
+sed -i "s|host    all             all             127.0.0.1/32            ident|host    all             all             127.0.0.1/32            md5|" /var/lib/pgsql/9.6/data/pg_hba.conf
+sed -i "s|host    all             all             ::1/128                 ident|host    all             all             ::1/128                 md5|" /var/lib/pgsql/9.6/data/pg_hba.conf
+echo -e "host    all             all             10.0.2.2/32               md5" >> /var/lib/pgsql/9.6/data/pg_hba.conf
 service postgresql-9.4 start
 su postgres -c "psql -U postgres -c \"CREATE USER \"entropy\" WITH PASSWORD 'secret';\""
 su postgres -c "psql -U postgres -c \"ALTER USER entropy WITH SUPERUSER;\""
