cc5e6a986371b459929a898093d0624126e3782b
Ammon Casey <ammon@caseyohana.com>
2017-12-01 10:23:51 -1000

    [scripts] remove some utils

--- script/entropy/utils.sh
+++ script/entropy/utils.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -eux
 
 echo '==> Installing utilities'
-yum --enablerepo=remi,remi-php71 -y install dkms patch net-tools selinux-policy-devel sudo nfs-utils openssh-clients rpcbind vim screen sendmail postfix git-core zsh
+yum --enablerepo=remi,remi-php71 -y install net-tools sudo nfs-utils rpcbind vim screen sendmail postfix git-core zsh
